a85c767e632f5f07976dd141f89e097b923a36c8
jimena <herrerajimena01@gmail.com>
2023-12-28 16:42:28 -0300

    se vio dom, eventos y creamos un cronometro

--- js/bom&dom.js
+++ js/bom&dom.js
@@ -28,3 +28,48 @@ setTimeout(()=>{
     console.log("despegueee");
     clearInterval(cuentaRegresiva);
 },11000)
+
+//DOM document object model
+
+let body=document.body;
+console.log(body);
+
+body.innerHTML+=`<h1 class="text-danger">Hola Mundo</h1>`
+body.innerHTML+=`<p class="bg-dark text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, quis?</p>`
+
+//pasandole el nombre de la etiqueta html
+//let h2=document.querySelector("h2")
+//console.log(h2);
+
+//pasandole el id
+
+let h2TCH=document.querySelector("#h2ToChange")
+
+console.log(h2TCH);
+
+let h2=document.getElementById("h2ToChange")
+console.log(h2);
+
+h2.textContent="H2 modificado"
+h2.classList.add("bg-secondary","text-dark");
+console.log(h2.classList);
+
+h2.classList.remove("bg-secondary");
+h2.classList.replace("text-dark","text-danger");
+
+let divPadre=document.getElementById("divPadre");
+
+console.log(divPadre);
+
+
+function insertarCard() {
+    divPadre.innerHTML+=` <div class="card my-5" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    </div>`
+    
+}
